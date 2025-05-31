a74c0665b8ea11092bad27dcc59f4cb18150e661
Khushansh <khushanshbaliyan@gmail.com>
2025-05-31 10:59:55 +0530

    Updated tweet button icon to Font Awesome's new branding, added functionality to post quotes on x.com, and improved error handling in fetchData function. Adjusted tweet button styles for better visibility.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// Initialize Variables
 const quoteContainer =  document.querySelector('#quote');
 const authorContainer =  document.querySelector('#author');
 const quoteGeneratorContainer =  document.querySelector('#new-quote');
+const tweetBtn = document.querySelector('#tweet-quote');
 
+// fetch Data
 async function fetchData(){
     const url = 'https://api.api-ninjas.com/v1/quotes';
     try{
@@ -13,7 +16,7 @@ async function fetchData(){
         contentType: 'application/json',
     })
     if(!response.ok){
-        throw new Error(console.log('error',Error));
+        throw new Error(console.log('HTTP error!',response.status));
     }
     const data = await response.json();
     console.log(data)
@@ -24,11 +27,37 @@ async function fetchData(){
     }
 
 }
-
+// functioning of button
 quoteGeneratorContainer.addEventListener('click', () =>{
-    
+    quoteDisplayer()
 })
 
+// functionality of xBtn 
+tweetBtn.addEventListener('click', () =>{
+    postOnxCom();
+})
+// Post on x.com
+function postOnxCom(){
+    const quote = quoteContainer.innerHTML;
+    const author = authorContainer.innerHTML;
+    const post  =`${quote} ${author}`
+    const tweetURl = `https://x.com/intent/tweet?text=${encodeURIComponent(post)}`;
+    window.open(tweetURl, '_blank');
+}
+
+// Display Quote
+async function quoteDisplayer() {
+try{
+    const data = await fetchData();
+    quoteContainer.innerHTML = data[0].quote;
+    authorContainer.innerHTML = data[0].author;
+}
+    catch(error){
+        console.log('error', error)
+    }
+  
+
+}
 
 
 fetchData()
